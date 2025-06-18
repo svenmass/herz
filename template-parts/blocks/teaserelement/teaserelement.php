02efa575794b61cd9cc595c82467a090140a88d4
svenmass <sven@massanneck.de>
2025-06-18 19:11:58 +0200

    teaserelement angepasst

--- template-parts/blocks/teaserelement/teaserelement.php
+++ template-parts/blocks/teaserelement/teaserelement.php
@@ -1,6 +1,5 @@
 <?php
-
-/* Teaserelement Block Template. */
+/* Teaserelement Block Template mit BEM-Klassen */
 
 $teaserbackgroundimageleft = get_field('teaserelement_hintergrundbild_links');
 $teaserueberschriftleft = get_field('teaserelement_uberschrift_links');
@@ -11,28 +10,44 @@ $teaserbackgroundimageright = get_field('teaserelement_hintergrundbild_rechts');
 $teaserueberschriftright = get_field('teaserelement_uberschrift_rechts');
 $teasersubheadlineright = get_field('teaserelement_subheadline_rechts');
 $teaserlinkright = get_field('teaserelement_link_rechts');
-
 ?>
-<div class="row space-between">
-    <div class="col-md-6 col-sm-12 col-has">
-        <div class="teaserelement-box flex-box-column has-box-padding has-box-bg" style="background-image: url('<?php echo $teaserbackgroundimageleft; ?>');" alt="Hintergrundbild des linken Teaserelements">
-            <h2><?php echo $teaserueberschriftleft; ?></h2>
-            <div class="bottom-link-wrap">
-                <a href="<?php echo $teaserlinkleft; ?>" aria-label="<?php echo $teasersubheadlineleft; ?>">
-                    <?php echo $teasersubheadlineleft; ?>
-                </a>
+
+<div class="teaserbox-row">
+
+    <!-- Linker Teaser -->
+    <div class="teaserbox-col">
+        <div class="teaserbox" role="region" aria-label="<?php echo esc_attr($teaserueberschriftleft); ?>">
+            <?php if ($teaserbackgroundimageleft): ?>
+                <img src="<?php echo esc_url($teaserbackgroundimageleft); ?>" alt="" aria-hidden="true" class="teaserbox__image" />
+            <?php endif; ?>
+            <div class="teaserbox__overlay"></div>
+            <div class="teaserbox__content">
+                <h2 class="teaserbox__title"><?php echo wp_kses_post($teaserueberschriftleft); ?></h2>
+                <div class="teaserbox__link-wrap">
+                    <a href="<?php echo esc_url($teaserlinkleft); ?>" class="teaserbox__link" aria-label="<?php echo esc_attr($teasersubheadlineleft); ?>">
+                        <?php echo wp_kses_post($teasersubheadlineleft); ?>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="col-md-6 col-sm-12 col-has">
-        <div class="teaserelement-box flex-box-column has-box-padding has-box-bg" style="background-image: url('<?php echo $teaserbackgroundimageright; ?>');" alt="Hintergrundbild des rechten Teaserelements">
-            <h2><?php echo $teaserueberschriftright; ?></h2>
-            <div class="bottom-link-wrap">
-                <a href="<?php echo $teaserlinkright; ?>" aria-label="<?php echo $teasersubheadlineright; ?>">
-                    <?php echo $teasersubheadlineright; ?>
-                </a>
+    <!-- Rechter Teaser -->
+    <div class="teaserbox-col">
+        <div class="teaserbox" role="region" aria-label="<?php echo esc_attr($teaserueberschriftright); ?>">
+            <?php if ($teaserbackgroundimageright): ?>
+                <img src="<?php echo esc_url($teaserbackgroundimageright); ?>" alt="" aria-hidden="true" class="teaserbox__image" />
+            <?php endif; ?>
+            <div class="teaserbox__overlay"></div>
+            <div class="teaserbox__content">
+                <h2 class="teaserbox__title"><?php echo wp_kses_post($teaserueberschriftright); ?></h2>
+                <div class="teaserbox__link-wrap">
+                    <a href="<?php echo esc_url($teaserlinkright); ?>" class="teaserbox__link" aria-label="<?php echo esc_attr($teasersubheadlineright); ?>">
+                        <?php echo wp_kses_post($teasersubheadlineright); ?>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
+
 </div>
